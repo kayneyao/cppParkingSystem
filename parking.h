fcc7e7bc8d34608e36cee6f92f4bf8f86388b471
kayneyao <kayne0222@gmail.com>
2021-05-15 13:56:08 +0800

    Revert "Revert "Update parking.h""
    This reverts commit 329f0da91555257c3bb5cc1831952744c4f0cf2a.

--- parking.h
+++ parking.h
@@ -1,10 +1,42 @@
 #include<iostream>
 #include<vector>
 
+using namespace std;
+
 class ParkingSystem {
-public:  
-  ParkingSystem(int big, int medium, int small) {}
-  
+	vector<int> slots;
+	
+	int carId = 0;
+	int price = 0;
+	
+	int privateData = 888;
+	
 
-
+	
+	public: 
+		int publicData = 666; 
+	  ParkingSystem(int big, int medium, int small, int priceIn) {
+		slots.push_back(0);
+		slots.push_back(big); 
+		slots.push_back(medium); 
+		slots.push_back(small); 
+		
+		price = priceIn;
+	  }
+	  
+	void Info(){
+	sdf90snrwehrhwenrwherh
+	}
+	 
+	 bool parkCar(int carType){
+	 	
+	 }
+	 
+	 int spaceNum(int ID){
+	 		
+	 }
+	 int parkcost(int ID){
+	 	
+	 }
+	 
 };
